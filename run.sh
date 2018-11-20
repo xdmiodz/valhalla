308c5123f60d48e0f954e8d368c7fe5a793551af
Dmitry Odzerikho <xdmiodz@swatmobile.io>
2018-11-20 10:38:49 +0800

    use OSM env to get map

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+# set OSM_MAP variable to point path to used osm map
 set -e
 
-mkdir -p ${PWD}/tiles
-wget https://download.geofabrik.de/asia/malaysia-singapore-brunei-latest.osm.pbf
 valhalla_build_config --mjolnir-tile-dir ${PWD}/tiles --mjolnir-tile-extract ${PWD}/tiles.tar --mjolnir-timezone ${PWD}/tiles/timezones.sqlite --mjolnir-admin ${PWD}/tiles/admins.sqlite > valhalla.json
-valhalla_build_tiles -c valhalla.json malaysia-singapore-brunei-latest.osm.pbf
+valhalla_build_tiles -c valhalla.json ${OSM_MAP}
 find tiles | sort -n | tar cf tiles.tar --no-recursion -T -
-rm -rf /valhalla/tiles /valhalla/malaysia-singapore-brunei-latest.osm.pbf
 valhalla_service valhalla.json 4
